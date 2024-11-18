6bd9b2e338bc7ff0ee1fe88c2f8466febab0058b
iamsufiyan560 <msufiyanhusen@gmail.com>
2024-11-18 23:41:19 +0530

    create auth pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,48 @@
 import "./App.css";
+import { Navigate, Route, Routes } from "react-router-dom";
+
+import toast from "react-hot-toast";
+import { useQuery } from "@tanstack/react-query";
+import { axiosInstance } from "./lib/axios";
+
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/auth/LoginPage";
+import SignUpPage from "./pages/auth/SignUpPage";
 
 function App() {
+  const { data: authUser, isLoading } = useQuery({
+    queryKey: ["authUser"],
+    queryFn: async () => {
+      try {
+        const res = await axiosInstance.get("/auth/me");
+        return res.data;
+      } catch (err) {
+        if (err.response && err.response.status === 401) {
+          return null;
+        }
+        toast.error(err.response.data.message || "Something went wrong");
+      }
+    },
+  });
+
+  if (isLoading) return null;
+
   return (
     <>
-      <h1>hello</h1>
+      <Routes>
+        <Route
+          path="/"
+          element={authUser ? <HomePage /> : <Navigate to={"/login"} />}
+        />
+        <Route
+          path="/signup"
+          element={!authUser ? <SignUpPage /> : <Navigate to={"/"} />}
+        />
+        <Route
+          path="/login"
+          element={!authUser ? <LoginPage /> : <Navigate to={"/"} />}
+        />
+      </Routes>
     </>
   );
 }
